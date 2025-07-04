e6e14c1aba9b709b79e86c819fc2456418531e59
Claudio-Gonzalez-X <cl.gonzalez2@duocuc.cl>
2025-07-03 23:14:03 -0400

    Delete notificationservice directory

--- notificationservice/notificationservice/src/main/java/com/perfulandia/notificationservice/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.perfulandia.notificationservice.repository;
-
-import com.perfulandia.notificationservice.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
